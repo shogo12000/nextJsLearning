a7eb3338699301b8f5d2df4eab9fbf3f865d361c
sidney <shogo12000@hotmail.com>
2025-03-28 06:23:39 -0700

    fix invoices

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -1,8 +1,11 @@
 'use client';
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/navigation';
  
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  
   function handleSearch(term: string) {
     console.log(term);
   }
