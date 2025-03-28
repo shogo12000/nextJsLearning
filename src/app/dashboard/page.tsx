3bd000c82f8d62ce94b060e2d73d017a535c422b
sidney <shogo12000@hotmail.com>
2025-03-28 03:13:12 -0700

    fetch dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 // import { Card } from "../ui/dashboard/cards";
-import { fetchRevenue } from "../lib/data";
+// import { fetchRevenue } from "../lib/data";
 // import { lusitana } from "@/app/ui/fonts";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
+//   const revenue = await fetchRevenue();
   return (
     <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
       <RevenueChart />
