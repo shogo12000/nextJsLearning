b96f605c26c20e3e64f11a993cf9b3950396cde7
sidney <shogo12000@hotmail.com>
2025-03-28 03:59:46 -0700

    fetch data card Component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,23 @@
 // import { Card } from "../ui/dashboard/cards";
 // import { fetchRevenue } from "../lib/data";
 // import { lusitana } from "@/app/ui/fonts";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 
 export default async function Page() {
-//   const revenue = await fetchRevenue();
+  //   const revenue = await fetchRevenue();
   return (
-    <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-      <RevenueChart />
-      {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
-      <LatestInvoices />
+    <>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <CardWrapper />
+      </div>
 
-    </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart />
+
+        <LatestInvoices />
+      </div>
+    </>
   );
 }
