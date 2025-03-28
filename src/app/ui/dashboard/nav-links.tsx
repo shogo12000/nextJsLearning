5a0e94a0efc9679d636b0b51234581feacedea75
sidney <shogo12000@hotmail.com>
2025-03-28 01:09:58 -0700

    configurate postgres

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,29 +1,16 @@
 "use client";
-
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from "@heroicons/react/24/outline";
+ 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { navLinks } from "@/app/lib/placeholder-data";
 
-const links = [
-  { name: "Home", href: "/dashboard", icon: HomeIcon },
-  {
-    name: "Invoices",
-    href: "/dashboard/invoices",
-    icon: DocumentDuplicateIcon,
-  },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
-];
 
 export default function NavLinks() {
   const pathname = usePathname();
 
   return (
     <>
-      {links.map((link ) => {
+      {navLinks.map((link ) => {
         const LinkIcon = link.icon;
         return (
           <Link
