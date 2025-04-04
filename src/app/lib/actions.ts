7537557b153ef7265aec5fcc2c86309a57fa2ad8
sidney <shogo12000@hotmail.com>
2025-04-04 08:40:19 -0700

    validate form with slint

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -53,6 +53,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error("Error updating invoice:", error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
@@ -97,6 +98,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
   } catch (error) {
+    console.error("Error updating invoice:", error);
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
